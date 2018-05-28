a37d06485dc0da5872cb9d6650e9b6ef5d25d7de
SundownDEV <raphael@crvx.fr>
2018-05-28 23:11:58 +0200

    Refactor entities

---
+++ src/Entity/Question.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Validator\Constraints as Assert;
+use ApiPlatform\Core\Annotation\ApiSubresource;
+
+/**
+ * @ApiResource()
+ * @ORM\Table(name="question")
+ * @ORM\Entity(repositoryClass="App\Repository\QuestionRepository")
+ */
+class Question
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $text;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Response", mappedBy="question")
+     * @ApiSubresource()
+     */
+    private $responses;
+
+    /**
+     * @ORM\OneToOne(targetEntity="App\Entity\Response", mappedBy="child", cascade={"persist", "remove"})
+     * @ApiSubresource()
+     */
+    private $response;
+
+    public function __construct()
+    {
+        $this->responses = new ArrayCollection();
+        $this->date = new \DateTime();
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Response[]
+     */
+    public function getResponses(): Collection
+    {
+        return $this->responses;
+    }
+
+    public function addResponse(Response $response): self
+    {
+        if (!$this->responses->contains($response)) {
+            $this->responses[] = $response;
+            $response->setQuestion($this);
+        }
+
+        return $this;
+    }
+
+    public function removeResponse(Response $response): self
+    {
+        if ($this->responses->contains($response)) {
+            $this->responses->removeElement($response);
+            // set the owning side to null (unless already changed)
+            if ($response->getQuestion() === $this) {
+                $response->setQuestion(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getResponse(): ?Response
+    {
+        return $this->response;
+    }
+
+    public function setResponse(?Response $response): self
+    {
+        $this->response = $response;
+
+        // set (or unset) the owning side of the relation if necessary
+        $newChild = $response === null ? null : $this;
+
+        /*if ($newChild !== $response->getChild()) {
+            $response->setChild($newChild);
+        }*/
+
+        return $this;
+    }
+}
---
+++ src/Entity/Response.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+use Symfony\Component\Validator\Constraints as Assert;
+use ApiPlatform\Core\Annotation\ApiSubresource;
+
+/**
+ * @ApiResource()
+ * @ORM\Table(name="response")
+ * @ORM\Entity(repositoryClass="App\Repository\ResponseRepository")
+ */
+class Response
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $text;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Question", inversedBy="responses")
+     */
+    private $question;
+
+    /**
+     * @ORM\OneToOne(targetEntity="App\Entity\Question", inversedBy="response", cascade={"persist", "remove"})
+     */
+    private $child;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getQuestion(): ?Question
+    {
+        return $this->question;
+    }
+
+    public function setQuestion(?Question $question): self
+    {
+        $this->question = $question;
+
+        return $this;
+    }
+
+    public function getChild(): ?Question
+    {
+        return $this->child;
+    }
+
+    public function setChild(?Question $child): self
+    {
+        $this->child = $child;
+
+        return $this;
+    }
+}
