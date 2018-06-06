449399a1834e48f36b2dc6f0d28baca3e2eff6b6
ayshiff <remi.d45@gmail.com>
2018-06-07 00:43:56 +0200

    Add MusicContainer -> wrap App for music continous rendering

--- client/src/components/Router.js
+++ client/src/components/Router.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Home from './Home'
-import App from './App'
+import MusicContainer from './MusicContainer'
 
 const Router = () => (
     <main>
         <Switch>
             <Route exact path='/' component={Home}/>
-            <Route path='/app' component={App}/>
+            <Route path='/app' component={MusicContainer}/>
         </Switch>
     </main>
 );
